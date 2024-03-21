56482a1ebfb7ac4ed9ad8314eafaa2d991d648a5
NahidAbdulazimov0320 <nabdulazimov14014@ada.edu.az>
2024-03-21 12:31:14 +0400

    service, controller, repository

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -9,7 +9,7 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private long studentId;
+    private Long Id;
 
     private String name;
     
@@ -18,6 +18,30 @@ public class Student {
 
     public Student(String name){
         name = this.name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long studentId) {
+        this.Id = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     
